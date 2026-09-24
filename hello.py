450c3987496438f2ac9425042dde66d7b25471b7
Yanth1766 <yanth.dev@gmail.com>
2026-09-24 11:33:21 +0630

    Add age input to hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ name = input("What is your name? ")
 
 print("Hello," name)
 print("Welcome to my Python journey!")
+
+age = int(input("How old are you? ")
+
+print(f"Your are {age} years old.")
+print("I am learning Python on GitHub!")
