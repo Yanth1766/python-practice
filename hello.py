718f92563a7dbcf9f67080ce54e6c39538cba905
Yanth1766 <yanth.dev@gmail.com>
2026-09-24 13:17:39 +0630

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ name = input("What is your name? ")
 print("Hello," name)
 print("Welcome to my Python journey!")
 
-age = int(input("How old are you? ")
+age = int(input("How old are you? "))
 
 print(f"Your are {age} years old.")
 print("I am learning Python on GitHub!")
